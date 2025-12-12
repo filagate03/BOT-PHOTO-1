75e2557569c23b42db97017d0210f52d8660a313
filagate03 <alexfarley555@gmail.com>
2025-12-12 16:21:08 +0300

    add src/bot_photo/keyboards/__init__.py

---
+++ src/bot_photo/keyboards/__init__.py
@@ -0,0 +1,25 @@
+from .common import (
+    admin_cancel_keyboard,
+    admin_main_keyboard,
+    admin_manage_user_keyboard,
+    agreement_keyboard,
+    faces_keyboard,
+    main_menu_keyboard,
+    orientation_keyboard,
+    prompt_templates_keyboard,
+    sessions_keyboard,
+    styles_keyboard,
+)
+
+__all__ = [
+    "admin_cancel_keyboard",
+    "admin_main_keyboard",
+    "admin_manage_user_keyboard",
+    "agreement_keyboard",
+    "faces_keyboard",
+    "main_menu_keyboard",
+    "orientation_keyboard",
+    "prompt_templates_keyboard",
+    "sessions_keyboard",
+    "styles_keyboard",
+]
